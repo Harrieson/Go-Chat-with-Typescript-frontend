f76785d8b46f1bc017574968826eb9ed03d2ef4c
Harrie_Mburu <40524846+Harrieson@users.noreply.github.com>
2024-01-14 18:59:55 +0300

    Add Gin-gonic Framework

---
+++ server/internal/user/user_handler.go
@@ -0,0 +1,24 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	Service
+}
+
+func NewHandler(s Service) *Handler {
+	return &Handler{
+		Service: s,
+	}
+}
+
+func (h *Handler) CreateUser(c *gin.Context) {
+	var u CreateUserReq
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+}
